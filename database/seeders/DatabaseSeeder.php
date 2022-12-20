b2df0b799e5eebf302d9770927897dcdcb9a97e6
SerlockWright <vohoangtin92@gmail.com>
2022-12-20 15:34:28 +0700

    authen breeze with role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(UserTableSeeder::class);
+        \App\Models\User::factory(8)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
