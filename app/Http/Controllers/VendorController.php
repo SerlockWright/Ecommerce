aef71a986649f428131c0bee56b2d49a072b6e3b
SerlockWright <104074047+SerlockWright@users.noreply.github.com>
2022-12-23 17:13:22 +0700

    Vendor (#5)
    * fix
    
    * vendor login

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class VendorController extends Controller
 {
     public function VendorDashboard()
     {
-        return view('vendor.vendor_dashboard');
+        return view('vendor.index');
+    } //end method
+
+    public function VendorLogin(){
+        return view('vendor.vendor_login');
+    }//end method
+
+    public function VendorDestroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/vendor/login');
     } //end method
 }
