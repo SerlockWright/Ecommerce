63f5692a54d1d1b5e5a13e4d72b6ee5abdd73420
SerlockWright <vohoangtin92@gmail.com>
2022-12-23 18:26:23 +0700

    vendor profile

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class VendorController extends Controller
 {
@@ -23,4 +24,34 @@ class VendorController extends Controller
         $request->session()->regenerateToken();
         return redirect('/vendor/login');
     } //end method
+
+    public function VendorProfile(){
+        $id = Auth::user()->id;
+        $vendorData = User::find($id);
+        return view('vendor.vendor_profile_view', compact('vendorData'));
+    }//End method
+
+    public function VendorProfileStore(Request $request){
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+        $data->vendor_join = $request->vendor_join;
+        $data->vendor_short_info = $request->vendor_short_info;
+        if($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/vendor_images/'.$data->photo));
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/vendor_images/'), $filename);
+            $data['photo'] = $filename;
+        }
+        $data->save();
+        $notification = array(
+            'message' => 'Vendor Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    }//End method
 }
--- resources/views/vendor/body/sidebar.blade.php
+++ resources/views/vendor/body/sidebar.blade.php
@@ -12,14 +12,14 @@
 	<!--navigation-->
 	<ul class="metismenu" id="menu">
 		<li>
-			<a href="{{ route('admin.dashboard') }}">
+			<a href="{{ route('vendor.dashboard') }}">
 				<div class="parent-icon"><i class='bx bx-home-circle'></i>
 				</div>
 				<div class="menu-title">Dashboard</div>
 			</a>
 		</li>
 			<li>
-				<a href="javascript:;" class="has-arrow">
+				<a href="" class="has-arrow">
 					<div class="parent-icon"><i class='bx bx-cookie'></i>
 					</div>
 					<div class="menu-title">Brand</div>
